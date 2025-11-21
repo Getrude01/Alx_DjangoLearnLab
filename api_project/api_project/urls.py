26bcb85001cfe33da41a53f98749241d8f5cfb41
Getrude <getrudengure6@gmail.com>
2025-11-21 15:57:19 +0300

    Add api_project content

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -1,13 +1,9 @@
+from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from api.views import BookList, BookViewSet # pyright: ignore[reportMissingImports]
-
-# Create router
-router = DefaultRouter()
-router.register(r'books_all', BookViewSet, basename='book_all')
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
-    # Router URLs for all CRUD operations
-    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    path('api/token/', obtain_auth_token, name='api-token'),
 ]
